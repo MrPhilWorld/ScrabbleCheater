f9f4eca1f9ffd4d5eba527dabf05adb67d6eb3fe
Phil Lafrance <phillafrance@phils-mbp.local>
2018-01-28 01:06:37 -0500

    Initial Commit

---
+++ Scrabble CheaterTests/Scrabble_CheaterTests.swift
@@ -0,0 +1,36 @@
+//
+//  Scrabble_CheaterTests.swift
+//  Scrabble CheaterTests
+//
+//  Created by Phil Lafrance on 2018-01-28.
+//  Copyright © 2018 Phil Lafrance. All rights reserved.
+//
+
+import XCTest
+@testable import Scrabble_Cheater
+
+class Scrabble_CheaterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
